491f526357f7002789e86b1fca1fb71af92ebdaa
jacobhuesman <jacob.huesman@ndsu.edu>
2017-10-01 00:29:21 -0500

    Transmitting and receiving data from the AX-12

--- main.c
+++ main.c
@@ -8,7 +8,17 @@
 
 
 unsigned int blink = 0;
-uint8_t packet[8]; //TODO figure out ideal packet size!!!
+
+
+uint8_t button_pressed;
+
+
+uint8_t tx_packet[20]; //TODO figure out ideal packet size
+uint8_t rx_packet[20];
+
+uint8_t tx_cb; // Current TX Byte
+uint8_t rx_cb;
+
 uint8_t packet_length = 8;
 
 void initialize(void)
@@ -37,35 +47,22 @@ void initialize(void)
     UCA0BR0 = 0x08;       // 1MHz 115200
     UCA0BR1 = 0x00;       // 1MHz 115200
     UCA0CTL1 &= ~UCSWRST; // Release USCI for operation (slau144j.pdf page 445)
-    //UC0IE |= UCA0RXIE;    // Enable USCI_A0 RX interrupt
-    //UC0IE |= UCA0TXIE;    // Enable USCI_A0 TX interrupt
+    UC0IE |= UCA0RXIE;    // Enable USCI_A0 RX interrupt
 
     // Configure Button
     P1REN |= BUTTON;
     P1OUT |= BUTTON;
     P1IES |= BUTTON;
     P1IE  |= BUTTON; //Enables the selector-mask for generating interrupts on the relevant pin
+    button_pressed = 0;
 
     // Initialize packet
-    packet[0] = 0xFF;
-    packet[1] = 0xFF;
+    tx_packet[0] = 0xFF;
+    tx_packet[1] = 0xFF;
+    tx_cb = 0;
 
     // Enable Interrupts
     __enable_interrupt();
-}
-
-int read_data(uint8_t id, uint8_t start_address, uint8_t data_length)
-{
-    packet_length = 8;
-
-    packet[2] = id;            // ID
-    packet[3] = 0x04;          // LENGTH
-    packet[4] = 0x02;          // INSTRUCTION
-    packet[5] = start_address; // PARAMETER1 (Start address)
-    packet[6] = data_length;   // PARAMETER2 (Length of data to be read)
-    packet[7] = calculate_checksum();
-
-    return 1;
 }
 
 uint8_t calculate_checksum(void)
@@ -74,22 +71,34 @@ uint8_t calculate_checksum(void)
     int i;
     for (i = 2; i < (packet_length - 1); i++)
     {
-        sum += packet[i];
+        sum += tx_packet[i];
     }
     return ~sum;
+}
+
+int read_data(uint8_t id, uint8_t start_address, uint8_t data_length)
+{
+    packet_length = 8;
+
+    tx_packet[2] = id;            // ID
+    tx_packet[3] = 0x04;          // LENGTH
+    tx_packet[4] = 0x02;          // INSTRUCTION
+    tx_packet[5] = start_address; // PARAMETER1 (Start address)
+    tx_packet[6] = data_length;   // PARAMETER2 (Length of data to be read)
+    tx_packet[7] = calculate_checksum();
+
+    return 1;
 }
 
 // Port 1 interrupt service routine
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1(void)
 {
+    button_pressed = 1;
+
     blink ^= 0x01;
     P1IFG &= ~BUTTON; // P1.3 IFG cleared
     P1OUT &= ~RXLED; // Clear the LEDs so they start in OFF state
-
-    // Send read request
-    read_data(0x01, 0x03, 0x01);
-    UC0IE |= UCA0TXIE;
 }
 
 // UART TX
@@ -97,22 +106,23 @@ __interrupt void Port_1(void)
 __interrupt void USCI0TX_ISR(void)
 {
     P1OUT |= TXLED;
-    UCA0TXBUF = 'A';
+
+    UCA0TXBUF = tx_packet[tx_cb++];
+    if (tx_cb >= packet_length)
+    {
+        tx_cb = 0;
+        UC0IE &= ~UCA0TXIE;
+    }
+
     P1OUT &= ~TXLED;
-    UC0IE &= ~UCA0TXIE;
 }
 
 // UART RX
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR(void)
 {
-   P1OUT |= RXLED;
-    if (UCA0RXBUF == 'a') // 'a' received?
-    {
-       //i = 0;
-       UC0IE |= UCA0TXIE; // Enable USCI_A0 TX interrupt
-      //UCA0TXBUF = string[i++];
-    }
+    P1OUT |= RXLED;
+    rx_packet[rx_cb++] = UCA0RXBUF;
     P1OUT &= ~RXLED;
 }
 
@@ -120,14 +130,25 @@ __interrupt void USCI0RX_ISR(void)
 int main(void)
 {
     initialize();
+    const uint8_t id = 1;
 
-
-    for (;;)
+    while(1)
     {
-        if(blink > 0)
+        if(button_pressed)
         {
-            P1OUT ^= RXLED;
-            __delay_cycles(100000); // SW Delay of 10000 cycles at 1Mhz
+            // Send read request
+            read_data(id, 0x03, 0x01);
+            P2OUT |= BIT0;
+            UC0IE |= UCA0TXIE;
+
+            while(UC0IE & UCA0TXIE);
+            __delay_cycles(88);
+
+            P2OUT &= ~BIT0;
+
+            __delay_cycles(1E7);
+            button_pressed = 0;
+            rx_cb = 0;
         }
     }
 
